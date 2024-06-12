c5586c35fc28b8e6b9e562a7eda5a83ab628face
georgetreger <georgetreger@gmail.com>
2024-06-12 09:12:59 -0700

    create header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,30 @@
-import './Header.css'
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className='header'>Header</div>
-  )
+    <div className="header">
+      <div className="wraper">
+        <div className="logo">
+          <Link className="link" to={"/"}>
+            <h2>Kwame</h2>
+            <h2>Booking</h2>
+          </Link>
+        </div>
+       
+          <ul className="menu">
+            <Link to={"/"}>
+              <li className="home">Home</li>
+            </Link>
+            <Link to={"/about"}>
+              <li className="about">About</li>
+            </Link>
+            <Link to={"/sign-in"}>
+              <li>Signin</li>
+            </Link>
+          </ul>
+       
+      </div>
+    </div>
+  );
 }
