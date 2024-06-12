011ac6935c5a815e2e7077347faed50b857f13da
georgetreger <georgetreger@gmail.com>
2024-06-12 07:54:11 -0700

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import SignIn from "./pages/signin/SignIn";
+import SignUp from "./pages/signup/SignUp";
+import Profile from "./pages/profile/Profile";
 
 
+ 
 
 
 function App() {
-  
-
   return (
     <div>
-    <h1>hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
