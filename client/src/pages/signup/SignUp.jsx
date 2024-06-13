a249374746921008eb96e0977e1bd37e59d442c3
georgetreger <georgetreger@gmail.com>
2024-06-13 10:55:59 -0700

    create signup ui page

--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -1,7 +1,22 @@
 import './SignUp.css'
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div className='signup'>SignUp</div>
-  )
+    <div className="signup">
+      <h1>  Sign Up</h1>
+      <form>
+        <input type="text" id="username" placeholder="Username" />
+        <input type="text" id="email" placeholder="Email" />
+        <input type="password" id="password" placeholder="Password" />
+         <button className='btn'>Sign Up</button>
+      </form>
+       <div className="account">
+         <p>Have an account?</p>
+          <Link to={'/sign-in'}>
+          <span className='spa'>Sign In</span>
+          </Link>
+       </div>
+    </div>
+  );
 }
